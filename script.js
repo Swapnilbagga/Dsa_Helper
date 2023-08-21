56ecb4bedd83d6469fe97273d2ac8d14ea7a513a
Swapnil Bagga <109497191+Swapnilbagga@users.noreply.github.com>
2023-08-21 15:51:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  // Add a delay before removing the typing animation class
+  setTimeout(function() {
+    $('.typing-text').removeClass('typing-text');
+  }, 2000);
+});
